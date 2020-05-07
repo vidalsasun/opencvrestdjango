0518824c024f3f7a40cb671fc502849e70942812
carlos <cmoreira@quonext-tur.com>
2020-05-07 13:18:29 +0200

    pytorch

--- OpenCVRestApi/models.py
+++ OpenCVRestApi/models.py
@@ -1,5 +1,7 @@
 # models.py
+from six import python_2_unicode_compatible
 from django.db import models
+
 class ImageCV(models.Model):
     name = models.CharField(max_length=250)
     base64 = models.TextField()
