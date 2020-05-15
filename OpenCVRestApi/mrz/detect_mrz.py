0b236f539c6cd661b5d9189e580b3794bf0e7892
carlos <cmoreira@quonext-tur.com>
2020-05-15 10:30:57 +0200

    tessdata

--- OpenCVRestApi/mrz/detect_mrz.py
+++ OpenCVRestApi/mrz/detect_mrz.py
@@ -126,9 +126,10 @@ def detect(base64img):
 				logger.error('tessdata folder:')
 				logger.error(PROJECT_ROOT)
 				logger.error('-------------------')
+
 				#pytesseract.pytesseract.tesseract_cmd = './.apt/usr/share/tessdata'
 				custom_config = r'-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890< --psm 6'
-				translate = pytesseract.image_to_string(image[y:y+h, x:x+w], lang='OCRB', config=custom_config)
+				translate = pytesseract.image_to_string(image[y:y+h, x:x+w], lang='deu', config=custom_config)
 				return translate
                 
 				#traduction_words.append(translate)
